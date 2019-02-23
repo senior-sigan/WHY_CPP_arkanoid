3192ad7bc27f95af943c33653113c02cab08bc8b
Ilya Siganov <ilya.blan4@gmail.com>
2019-02-23 14:04:27 +0600

    use new whycpp version managment

--- CMake/DownloadWhyCpp.cmake
+++ CMake/DownloadWhyCpp.cmake
@@ -4,8 +4,8 @@ project(whycpp-download NONE)
 
 include(ExternalProject)
 ExternalProject_Add(whycpp
-        GIT_REPOSITORY https://github.com/senior-sigan/WHY_CPP.git
-        GIT_TAG           master
+        URL               https://github.com/senior-sigan/WHY_CPP/archive/v1.1.tar.gz
+        URL_MD5           "da865208e655144ba6a12c3a8a9cf230"
         SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/whycpp-src"
         BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/whycpp-build"
         CONFIGURE_COMMAND ""
