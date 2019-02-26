4a5d1464ddeaebc87f9403097aac3c07ffdd58d3
Ilya Siganov <ilya.blan4@gmail.com>
2019-02-26 23:01:00 +0600

    update lib

--- CMake/DownloadWhyCpp.cmake
+++ CMake/DownloadWhyCpp.cmake
@@ -4,8 +4,8 @@ project(whycpp-download NONE)
 
 include(ExternalProject)
 ExternalProject_Add(whycpp
-        URL               https://github.com/senior-sigan/WHY_CPP/archive/v1.2.tar.gz
-        URL_MD5           "b939abb68ee66b17c462fce1e725db68"
+        URL               https://github.com/senior-sigan/WHY_CPP/archive/v1.3.tar.gz
+        URL_MD5           "bd086709a34fb5006dca8155526a8f5d"
         SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/whycpp-src"
         BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/whycpp-build"
         CONFIGURE_COMMAND ""
