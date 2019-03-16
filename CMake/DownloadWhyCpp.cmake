f26b5ad6f627568b65edfc7d49318546e8d1c421
Ilya Siganov <ilya.blan4@gmail.com>
2019-03-16 12:51:44 +0600

    update game and fix fps

--- CMake/DownloadWhyCpp.cmake
+++ CMake/DownloadWhyCpp.cmake
@@ -4,8 +4,8 @@ project(whycpp-download NONE)
 
 include(ExternalProject)
 ExternalProject_Add(whycpp
-        URL               https://github.com/senior-sigan/WHY_CPP/archive/v1.8.tar.gz
-        URL_MD5           "f196d8ef255a2c190b4682cae6ea0edb"
+        URL               https://github.com/senior-sigan/WHY_CPP/archive/v1.9.tar.gz
+        URL_MD5           "b81c7c5a4b17603841f9cdb96f0cd8bf"
         SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/whycpp-src"
         BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/whycpp-build"
         CONFIGURE_COMMAND ""
